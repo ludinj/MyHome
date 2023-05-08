d140e9ec40d09ff544e36817b3ee31f58a19263c
ludinj <borrego300@yahoo.com>
2023-05-08 17:48:08 -0400

    Add test log to check in production

--- src/app/components/nabvar/Navbar.tsx
+++ src/app/components/nabvar/Navbar.tsx
@@ -10,8 +10,9 @@ import Categories from './Categories';
 interface NavbarProps {
   currentUser?: SafeUser | null;
 }
-
 const Navbar: FC<NavbarProps> = ({ currentUser }) => {
+  console.log(currentUser);
+
   return (
     <div className='fixed w-full bg-white z-10 shadow-sm'>
       <div className='py-4 border-b-[1px]'>
